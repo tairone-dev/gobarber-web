22d03648ee4f06a551a416430c3b3fec564f7f35
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2020-04-21 10:34:48 -0300

    Added user getstate signed

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,16 +1,19 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-import AuthLayout from '../pages/_layouts/auth';
-import DefaultLayout from '../pages/_layouts/default';
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
+
+import store from '~/store';
 
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const { signed } = store.getState().auth;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
