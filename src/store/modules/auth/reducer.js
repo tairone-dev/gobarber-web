84c1cef2d8ba552d1c94270e69e3ca51fdacdad0
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2020-04-21 12:28:03 -0300

    Added signup request and success

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -13,9 +13,17 @@ export default function auth(state = INITIAL_STATE, action) {
         draft.loading = true;
         break;
       }
+      case '@auth/SIGN_UP_REQUEST': {
+        draft.loading = true;
+        break;
+      }
       case '@auth/SIGN_IN_SUCCESS': {
         draft.token = action.payload.token;
         draft.signed = true;
+        draft.loading = false;
+        break;
+      }
+      case '@auth/SIGN_UP_SUCCESS': {
         draft.loading = false;
         break;
       }
