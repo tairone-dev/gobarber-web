d650dccb6fd744eb386cc1c62b58b682c8aa6e40
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2020-04-21 12:28:16 -0300

    Added signup

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess, signFailure } from './actions';
+import { signInSuccess, signFailure, signUpSuccess } from './actions';
 
 export function* signIn({ payload }) {
   try {
@@ -31,4 +31,28 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    yield put(signUpSuccess());
+
+    history.push('/');
+  } catch (error) {
+    toast.error('Falha ao cadastrar novo usuário, verifique seus dados');
+
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
