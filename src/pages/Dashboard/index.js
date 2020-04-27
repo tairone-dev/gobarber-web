31a6667909f5627ff78cf90db489bc3cce9128c0
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2020-04-27 20:18:45 -0300

    Added logic to show schedule

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,40 +1,94 @@
-import React from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import {
+  format,
+  subDays,
+  addDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  isBefore,
+  isEqual,
+  parseISO,
+  setMilliseconds,
+} from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
+import { utcToZonedTime } from 'date-fns-tz';
 
-// import api from '~/services/api';
+import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
 export default function Dashboard() {
+  const [schedule, setSchedule] = useState([]);
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get('schedules', {
+        params: { date },
+      });
+
+      const { timeZone } = Intl.DateTimeFormat().resolvedOptions();
+
+      const data = range.map((hour) => {
+        const checkDate = setMilliseconds(
+          setSeconds(setMinutes(setHours(date, hour), 0), 0),
+          0
+        );
+        const compareDate = utcToZonedTime(checkDate, timeZone);
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find((appointment) => {
+            return isEqual(parseISO(appointment.date), compareDate);
+          }),
+        };
+      });
+      // Descobrir porque não está pegando a data que tem appointment
+      setSchedule(data);
+    }
+
+    loadSchedule();
+  }, [date]);
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handlePrevDay}>
           <MdChevronLeft size={36} color="#FFF" />
         </button>
-        <strong>22 de abril</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
           <MdChevronRight size={36} color="#FFF" />
         </button>
       </header>
 
       <ul>
-        <Time past>
-          <strong>08:00</strong>
-          <span>Tairone Livinalli</span>
-        </Time>
-        <Time available>
-          <strong>09:00</strong>
-          <span>Em aberto</span>
-        </Time>
-        <Time>
-          <strong>10:00</strong>
-          <span>André Tedesco</span>
-        </Time>
-        <Time>
-          <strong>11:00</strong>
-          <span>Maxsuel Silva</span>
-        </Time>
+        {schedule.map((time) => (
+          <Time key={time.time} past={time.past} available={!time.appointment}>
+            <strong>{time.time}</strong>
+            <span>
+              {time.appointment ? time.appointment.user.name : 'Em aberto'}
+            </span>
+          </Time>
+        ))}
       </ul>
     </Container>
   );
